35d6c6721d1897268712c62ce6d39205cabe9f28
Vlast001 <anfernoangel@gmail.com>
2023-09-18 14:39:38 +0300

    add files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+ 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+Random rnd = new Random();
+
+// app.MapGet("/", (HttpContext context) => context.Response.WriteAsync("<h2>Hello World</h2>"));
+// app.MapGet("/", () => $"<h2>Your randow number from 1 to 6: {rnd.Next(1, 6)}</h2>");
+app.MapGet("/", (HttpContext context) =>
+{
+    var randomNumber = rnd.Next(1, 6);
+    context.Response.ContentType = "text/html";
+    return context.Response.WriteAsync($"<h2>Your random number from 1 to 6: {randomNumber}</h2>");
+});
+
+app.Run();
